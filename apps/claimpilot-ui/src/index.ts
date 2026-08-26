71af6467e5f2184f0118957ffa9b9ed5b752b4c6
Tarun 69 <tarunagnihotri534@gmail.com>
2026-08-26 19:56:55 +0530

    Enable standalone mounting for React app on localhost:3000

--- apps/claimpilot-ui/src/index.ts
+++ apps/claimpilot-ui/src/index.ts
@@ -1,2 +1,15 @@
-// Entry point for ClaimPilot App
-import('./AppDescriptor');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+let rootElement = document.getElementById('root');
+if (!rootElement) {
+	rootElement = document.createElement('div');
+	rootElement.id = 'root';
+	document.body.appendChild(rootElement);
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
+
+export * from './AppDescriptor';
